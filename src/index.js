2d3fb1d9d7da6b945c8b34e069dafb6b0fccbb89
joyceliu449 <86019275+joyceliu449@users.noreply.github.com>
2022-10-12 17:23:53 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1 class="blue white">My name is Joyce</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+<p class="purple" > i like cats </p>
+
+`;
+console.log("hi");
+console.log("i found the console log");
